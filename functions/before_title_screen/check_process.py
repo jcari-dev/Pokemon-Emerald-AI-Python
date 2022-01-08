89841e07dbd880d8dac61b63dd2b4adb23295d30
jcari-dev <582119jc@gmail.com>
2022-01-07 21:44:40 -0500

    added basic functions, now it checks if the emulator is running or not

---
+++ functions/before_title_screen/check_process.py
@@ -0,0 +1,9 @@
+import psutil
+def check_if_process_alive(processName):
+    for proc in psutil.process_iter():
+        try:
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False;
---
+++ start_playing.py
@@ -0,0 +1,6 @@
+# import os
+# os.startfile(".\\rom\\rom.gba")
+
+# import psutil    
+# "someProgram" in (p.name() for p in psutil.process_iter())
+from functions.before_title_screen.check_process import check_if_process_alive
