a8bbad327fb1b8c9fc63dd8ddc43f1d980609f3c
jcari-dev <582119jc@gmail.com>
2022-01-17 21:31:10 -0500

    removed get movement function since it will be replaced

--- functions/in_game_actions/write_player_name.py
+++ functions/in_game_actions/write_player_name.py
@@ -48,130 +48,130 @@ import itertools
 # timeit.timeit(lambda: get_actions_to_write_1('Juan'))
 
 
-def get_movement_order(index_locations, character_list):
-    old_location = [0, 0]
-    beginning = [0, 0]
-    letter_sequence = []
-    all_caps = True
-    all_lower = False
-    numbers = False
+# def get_movement_order(index_locations, character_list):
+#     old_location = [0, 0]
+#     beginning = [0, 0]
+#     letter_sequence = []
+#     all_caps = True
+#     all_lower = False
+#     numbers = False
 
-    # r = 'right'
-    # l = 'left'
-    # u = 'up'
-    # d = 'down'
+#     # r = 'right'
+#     # l = 'left'
+#     # u = 'up'
+#     # d = 'down'
 
-    for index, location in enumerate(index_locations):
-        if location[0] > beginning[0]:
-            if location[0] >= 4:
-                if all_lower == False:
-                    all_lower = True
-                    all_caps = False
-                    numbers = False
-                    # old_location[0] = location[0]
-                    # location[0] -= 4
-                    letter_sequence.append(['backspace'])
-                else:
-                    print(location, old_location, beginning)
-                    print('skipped backspace because we aleady in lower case')
-            elif location[0] >= 8:
-                if numbers == False:
-                    numbers = True
-                    all_caps = False
-                    all_lower = False
-                    # old_location[0] = location[0]
-                    # location[0] -= 8
-                    letter_sequence.append(['backspace'])
-                else:
-                    print('skipped backspace because already in numbers')
-            move_right = abs(beginning[0] - location[0])
-            rs = ["down" for x in range(move_right)]
-            letter_sequence.append(rs)
-        else:
-            if location[0] >= 4:
-                if all_lower == False:
-                    all_lower = True
-                    all_caps = False
-                    numbers = False
-                    # old_location[0] = location[0]
-                    # location[0] -= 4
-                    letter_sequence.append(['backspace'])
-                else:
-                    print(location, old_location)
-                    print('skipped backspace because we already in lower case')
-            elif location[0] >= 8:
-                if numbers == False:
-                    numbers = True
-                    all_lower = False
-                    all_caps = False
-                    # old_location[0] = location[0]
-                    # location[0] -= 8
-                    letter_sequence.append(['backspace'])
-                else:
-                    print('skipped because we already in numbers')
-            move_left = abs(beginning[0] - location[0])
-            ls = ["up" for x in range(move_left)]
-            letter_sequence.append(ls)
+#     for index, location in enumerate(index_locations):
+#         if location[0] > beginning[0]:
+#             if location[0] >= 4:
+#                 if all_lower == False:
+#                     all_lower = True
+#                     all_caps = False
+#                     numbers = False
+#                     # old_location[0] = location[0]
+#                     # location[0] -= 4
+#                     letter_sequence.append(['backspace'])
+#                 else:
+#                     print(location, old_location, beginning)
+#                     print('skipped backspace because we aleady in lower case')
+#             elif location[0] >= 8:
+#                 if numbers == False:
+#                     numbers = True
+#                     all_caps = False
+#                     all_lower = False
+#                     # old_location[0] = location[0]
+#                     # location[0] -= 8
+#                     letter_sequence.append(['backspace'])
+#                 else:
+#                     print('skipped backspace because already in numbers')
+#             move_right = abs(beginning[0] - location[0])
+#             rs = ["down" for x in range(move_right)]
+#             letter_sequence.append(rs)
+#         else:
+#             if location[0] >= 4:
+#                 if all_lower == False:
+#                     all_lower = True
+#                     all_caps = False
+#                     numbers = False
+#                     # old_location[0] = location[0]
+#                     # location[0] -= 4
+#                     letter_sequence.append(['backspace'])
+#                 else:
+#                     print(location, old_location)
+#                     print('skipped backspace because we already in lower case')
+#             elif location[0] >= 8:
+#                 if numbers == False:
+#                     numbers = True
+#                     all_lower = False
+#                     all_caps = False
+#                     # old_location[0] = location[0]
+#                     # location[0] -= 8
+#                     letter_sequence.append(['backspace'])
+#                 else:
+#                     print('skipped because we already in numbers')
+#             move_left = abs(beginning[0] - location[0])
+#             ls = ["up" for x in range(move_left)]
+#             letter_sequence.append(ls)
 
-        if location[1] > beginning[1]:
-            if location[1] >= 4:
-                if all_lower == False:
-                    all_lower = True
-                    all_caps = False
-                    numbers = False
-                    # old_location[1] = location[1]
-                    # location[1] -= 4
-                    letter_sequence.append(['backspace'])
-                else:
-                    print('skipped backspace because we already in lower case')
-            elif location[1] >= 8:
-                if numbers == False:
-                    numbers = True
-                    all_lower = False
-                    all_caps = False
-                    # old_location[1] = location[1]
-                    # location[1] -= 8
-                    letter_sequence.append(['backspace'])
-                else:
-                    print('skipped backspace because we already in numbers')
-            move_up = abs(beginning[1] - location[1])
-            us = ['right' for x in range(move_up)]
-            letter_sequence.append(us)
-        else:
-            if location[1] >= 4:
-                if all_lower == False:
-                    all_lower = True
-                    all_caps = False
-                    numbers = False
-                    # old_location[1] = location[1]
-                    # location[1] -= 4
-                    letter_sequence.append(['backspace'])
-                else:
-                    print('skipped backspace because we already in lower case')
-            elif location[1] >= 8:
-                if numbers == False:
-                    numbers = True
-                    all_lower = False
-                    all_caps = False
-                    # old_location[1] = location[1]
-                    # location[1] -= 8
-                    letter_sequence.append(['backspace'])
-                else:
-                    print('skipped because we already in numbers')
-            move_down = abs(beginning[1] - location[1])
-            ds = ['left' for x in range(move_down)]
-            letter_sequence.append(ds)
-        old_location = location
-        beginning = old_location
-        print(location, old_location)
-        letter_sequence.append('x')
-        print(character_list[location[0]][location[1]])
-    flatted_sequence = list(itertools.chain(*letter_sequence))
-    print(flatted_sequence)
-    print(len(flatted_sequence))
-    flatted_sequence.append('enter')
-    flatted_sequence.append('x')
-    flatted_sequence.append('x')
+#         if location[1] > beginning[1]:
+#             if location[1] >= 4:
+#                 if all_lower == False:
+#                     all_lower = True
+#                     all_caps = False
+#                     numbers = False
+#                     # old_location[1] = location[1]
+#                     # location[1] -= 4
+#                     letter_sequence.append(['backspace'])
+#                 else:
+#                     print('skipped backspace because we already in lower case')
+#             elif location[1] >= 8:
+#                 if numbers == False:
+#                     numbers = True
+#                     all_lower = False
+#                     all_caps = False
+#                     # old_location[1] = location[1]
+#                     # location[1] -= 8
+#                     letter_sequence.append(['backspace'])
+#                 else:
+#                     print('skipped backspace because we already in numbers')
+#             move_up = abs(beginning[1] - location[1])
+#             us = ['right' for x in range(move_up)]
+#             letter_sequence.append(us)
+#         else:
+#             if location[1] >= 4:
+#                 if all_lower == False:
+#                     all_lower = True
+#                     all_caps = False
+#                     numbers = False
+#                     # old_location[1] = location[1]
+#                     # location[1] -= 4
+#                     letter_sequence.append(['backspace'])
+#                 else:
+#                     print('skipped backspace because we already in lower case')
+#             elif location[1] >= 8:
+#                 if numbers == False:
+#                     numbers = True
+#                     all_lower = False
+#                     all_caps = False
+#                     # old_location[1] = location[1]
+#                     # location[1] -= 8
+#                     letter_sequence.append(['backspace'])
+#                 else:
+#                     print('skipped because we already in numbers')
+#             move_down = abs(beginning[1] - location[1])
+#             ds = ['left' for x in range(move_down)]
+#             letter_sequence.append(ds)
+#         old_location = location
+#         beginning = old_location
+#         print(location, old_location)
+#         letter_sequence.append('x')
+#         print(character_list[location[0]][location[1]])
+#     flatted_sequence = list(itertools.chain(*letter_sequence))
+#     print(flatted_sequence)
+#     print(len(flatted_sequence))
+#     flatted_sequence.append('enter')
+#     flatted_sequence.append('x')
+#     flatted_sequence.append('x')
 
 
 def get_index_of_characters(name):
